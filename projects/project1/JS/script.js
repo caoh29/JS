cf8d532a4bb27b296a45296e6c15661a20b37252
Camilo Ordoñez <cronox20@gmail.com>
2023-02-28 12:07:13 -0500

    refactoring variables and naming due to best pract

--- projects/project1/JS/script.js
+++ projects/project1/JS/script.js
@@ -1,35 +1,48 @@
 "use strict";
 
+function changeTextContent(selector, sentence) {
+  document.querySelector(`${selector}`).textContent = `${sentence}`;
+}
+
+function changeBackgroundColor(selector, color) {
+  document.querySelector(`${selector}`).style.background = `${color}`;
+}
+
 function getRandomInt(max) {
   return Math.trunc(Math.random() * max + 1);
+}
+
+function updateScoreAndMessage(value) {
+  if (score > 1) {
+    changeTextContent(".message", value);
+    score--;
+    changeTextContent(".score", score);
+  } else {
+    changeTextContent(".message", loosingMessage);
+    changeTextContent(".score", 0);
+    changeBackgroundColor("body", gameLostColor);
+  }
 }
 
 let randomNumber = getRandomInt(20);
 let score = 20;
 let highscore = 0;
 
-function changeTextContent(selector, sentence) {
-  document.querySelector(`${selector}`).textContent = `${sentence}`;
-}
+const guessHigherNumber = "Too High!";
+const guessLowerNumber = "Too Low!";
 
-function prompt(value) {
-  if (score > 1) {
-    changeTextContent(".message", value);
-    score--;
-    changeTextContent(".score", score);
-  } else {
-    changeTextContent(".message", "You Lost :(");
-    changeTextContent(".score", 0);
-    document.querySelector("body").style.background = "red";
-  }
-}
+const loosingMessage = "You Lost :(";
+
+const defaultGameColor = "salmon";
+const gameLostColor = "red";
+const gameWonColor = "green";
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = document.querySelector(".guess").value;
 
   if (guess == randomNumber) {
     changeTextContent(".message", "Correct Number!");
-    document.querySelector("body").style.background = "green";
+    changeBackgroundColor("body", gameWonColor);
     changeTextContent(".number", randomNumber);
     changeTextContent("h1", "YOU GUESSED!");
     if (score > highscore) {
@@ -37,9 +50,9 @@ document.querySelector(".check").addEventListener("click", function () {
       changeTextContent(".highscore", highscore);
     }
   } else if (guess > randomNumber) {
-    prompt("Too High!");
+    updateScoreAndMessage(guessHigherNumber);
   } else if (guess < randomNumber) {
-    prompt("Too Low!");
+    updateScoreAndMessage(guessLowerNumber);
   }
 });
 
@@ -50,6 +63,6 @@ document.querySelector(".again").addEventListener("click", function () {
   changeTextContent(".score", score);
   changeTextContent(".number", "?");
   document.querySelector(".guess").value = "";
-  document.querySelector("body").style.background = "salmon";
+  changeBackgroundColor("body", defaultGameColor);
   changeTextContent("h1", "Guess My Number!");
 });
