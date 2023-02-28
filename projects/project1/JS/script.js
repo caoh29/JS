08400262607559bcc690c0db67efd8073d596759
Camilo Ordoñez <cronox20@gmail.com>
2023-02-28 10:18:05 -0500

    refactoring by adding repeated code into function

--- projects/project1/JS/script.js
+++ projects/project1/JS/script.js
@@ -8,43 +8,48 @@ let randomNumber = getRandomInt(20);
 let score = 20;
 let highscore = 0;
 
+function changeTextContent(selector, sentence) {
+  document.querySelector(`${selector}`).textContent = `${sentence}`;
+}
+
+function prompt(value) {
+  if (score > 1) {
+    changeTextContent(".message", value);
+    score--;
+    changeTextContent(".score", score);
+  } else {
+    changeTextContent(".message", "You Lost :(");
+    changeTextContent(".score", 0);
+    document.querySelector("body").style.background = "red";
+  }
+}
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = document.querySelector(".guess").value;
 
-  function prompt(value) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = `Too ${value}!`;
-      score--;
-      document.querySelector(".score").textContent = `${score}`;
-    } else {
-      document.querySelector(".message").textContent = "You Lost :(";
-      document.querySelector(".score").textContent = 0;
-      document.querySelector("body").style.background = "red";
-    }
-  }
   if (guess == randomNumber) {
-    document.querySelector(".message").textContent = "Correct Number!";
+    changeTextContent(".message", "Correct Number!");
     document.querySelector("body").style.background = "green";
-    document.querySelector(".number").textContent = `${randomNumber}`;
-    document.querySelector("h1").textContent = `YOU GUESSED!`;
+    changeTextContent(".number", randomNumber);
+    changeTextContent("h1", "YOU GUESSED!");
     if (score > highscore) {
       highscore = score;
-      document.querySelector(".highscore").textContent = highscore;
+      changeTextContent(".highscore", highscore);
     }
   } else if (guess > randomNumber) {
-    prompt("High");
+    prompt("Too High!");
   } else if (guess < randomNumber) {
-    prompt("Low");
+    prompt("Too Low!");
   }
 });
 
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   randomNumber = getRandomInt(20);
-  document.querySelector(".message").textContent = "Start guessing...";
-  document.querySelector(".score").textContent = `${score}`;
-  document.querySelector(".number").textContent = `?`;
+  changeTextContent(".message", "Start guessing...");
+  changeTextContent(".score", score);
+  changeTextContent(".number", "?");
   document.querySelector(".guess").value = "";
   document.querySelector("body").style.background = "salmon";
-  document.querySelector("h1").textContent = `Guess My Number!`;
+  changeTextContent("h1", "Guess My Number!");
 });
